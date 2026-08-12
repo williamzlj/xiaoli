c1a673af8ef37a708601c72ad18b176919c6dd5e
williamzlj <williamzlj@126.com>
2026-08-12 21:57:44 +0800

    调整默认圆内字号为28；新增图片导出介绍文档

--- src/types/index.ts
+++ src/types/index.ts
@@ -118,22 +118,22 @@ export const PRESET_NOTE_COLORS: ColorScheme[] = [
   { name: '黑色', colors: ['#000000'] },
 ];
 
-export const DEFAULT_DATE_COLUMN_WIDTH = 110;/*日期列默认宽度,日期列宽*/
-export const DEFAULT_TABLE_HEIGHT = 70;/*表格高度*/
+export const DEFAULT_DATE_COLUMN_WIDTH = 127;/*日期列默认宽度,日期列宽*/
+export const DEFAULT_TABLE_HEIGHT = 79;/*表格高度*/
 export const DEFAULT_NOTE_COLUMN_WIDTH = 250;/*备注列宽*/
 export const DEFAULT_NOTE_COLOR = '#dc2626';
 export const DEFAULT_WEEK_NOTE_COLOR = '#000000';
-export const DEFAULT_NOTE_FONT_SIZE = 16;
-export const DEFAULT_DATE_FONT_SIZE = 26;/*日期字号*/
-export const DEFAULT_TITLE_FONT_SIZE = 48;
-export const DEFAULT_SUBTITLE_FONT_SIZE = 20;
+export const DEFAULT_NOTE_FONT_SIZE = 16;//备注字号
+export const DEFAULT_DATE_FONT_SIZE = 30;/*日期字号*/
+export const DEFAULT_TITLE_FONT_SIZE = 48;//标题字号
+export const DEFAULT_SUBTITLE_FONT_SIZE = 26;//副标题字号
 export const DEFAULT_WEEK_START_NUMBER = 1;
 export const DEFAULT_SHOW_SCREENSHOT_MODE = false;
 export const DEFAULT_TITLE_BG_COLOR = '#ffffff';
 export const DEFAULT_TITLE_TEXT_COLOR = '#000000';
-export const DEFAULT_COURSE_NUMBER_WIDTH = 32;/*圆圈宽度*/
-export const DEFAULT_COURSE_NUMBER_HEIGHT = 32;/*圆圈高度*/
-export const DEFAULT_COURSE_NUMBER_FONT_SIZE = 25;/*圆圈字体大小，圈内字号*/
+export const DEFAULT_COURSE_NUMBER_WIDTH = 36;/*圆圈宽度*/
+export const DEFAULT_COURSE_NUMBER_HEIGHT = 36;/*圆圈高度*/
+export const DEFAULT_COURSE_NUMBER_FONT_SIZE = 28;/*圆圈字体大小，圈内字号*/
 export const DEFAULT_COURSE_GROUPS: CourseGroup[] = [
   { id: 'fri', name: '周五班', color: '#10b981', courseDates: [] },
   { id: 'sat', name: '周六班', color: '#3b82f6', courseDates: [] },
